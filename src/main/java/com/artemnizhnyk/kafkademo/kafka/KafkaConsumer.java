e152fda8ebee9db518a3dc89e5f48a5d49402ed7
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-01-28 20:10:38 +0100

    logger was changed and kafka base config was corrected

--- src/main/java/com/artemnizhnyk/kafkademo/kafka/KafkaConsumer.java
+++ src/main/java/com/artemnizhnyk/kafkademo/kafka/KafkaConsumer.java
@@ -1,10 +1,11 @@
 package com.artemnizhnyk.kafkademo.kafka;
 
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+@Log4j2
 @Service
 public class KafkaConsumer {
 
